1335b72f96020607ab96b1c424540cb09e1371b2
Shaun Davis <shapadavis@gmail.com>
2022-08-10 05:16:18 -0500

    Updated scripts

---
+++ xrandr-screen-layout.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DP2 --mode 1680x1050 --pos 1920x0 --rotate normal --output DP1 --off --output DP3
+--primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
